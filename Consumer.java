4341aa2a65ffdfc5878d3a49ff398c95eb67c2bc
Sena Aksakallı <122912964+senaaksakalli@users.noreply.github.com>
2025-07-03 11:12:31 +0300

    Update Consumer.java

--- Consumer.java
+++ Consumer.java
@@ -1,5 +1,7 @@
+
 package org.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
@@ -9,33 +11,32 @@ import java.util.Properties;
 
 public class Consumer {
 
-    public static void main(String[] args) {
-        // Set the properties for Kafka Consumer
+    public static <JsonProcessingException> void main(String[] args) {
         Properties properties = new Properties();
         properties.put("bootstrap.servers", "localhost:9092");
         properties.put("group.id", "test-group");
         properties.put("key.deserializer", StringDeserializer.class.getName());
         properties.put("value.deserializer", StringDeserializer.class.getName());
 
-        // Create the Kafka Consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
 
         try {
-            // Subscribe to the topic
             consumer.subscribe(Collections.singletonList("test-topic"));
 
-            // Poll for new messages
             while (true) {
-                var records = consumer.poll(Duration.ofSeconds(2));  // Poll for 2 seconds
-
-                records.forEach(record -> {
-                    System.out.println("Received message: Key = " + record.key() + ", Value = " + record.value());
-                });
+                var records = consumer.poll(Duration.ofSeconds(2));
+                for (var record : records) {
+                    try {
+                        Student student = Student.fromJson(record.value());
+                        System.out.println("Alındı: " + student);
+                    } catch (JsonProcessingException e) {
+                        System.out.println("JSON çözümleme hatası: " + record.value());
+                    }
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            // Close the consumer
             consumer.close();
         }
     }
